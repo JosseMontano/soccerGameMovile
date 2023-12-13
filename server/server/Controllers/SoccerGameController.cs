1c88783e0fa9b59126b875d326f685070c2c87d1
JosseMontano <josemariazambranamontano123@gmail.com>
2023-12-12 23:28:59 -0400

    bk:the controllers is working!

--- server/server/Controllers/SoccerGameController.cs
+++ server/server/Controllers/SoccerGameController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using server.Model;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace server.Controllers
 {
@@ -18,28 +20,79 @@ namespace server.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            var soccergames = _db.Partidos;
+            var soccergames = _db.Partidos.Join(_db.Usuarios,
+                game => game.UsuarioId,
+                user => user.UsuarioId,
+                (game, user)=> new
+                {
+                    PartidoId= game.PartidoId,
+                    MaximoJugadores = game.MaximoJugadores,
+                    TipoCancha = game.TipoCancha,
+                    Fecha = game.Fecha,
+                    Hora = game.Hora,
+                    UsuarioId = game.UsuarioId,
+                    NombreUsuario = user.NombreUsuario,
+                }
+                );
             return Ok(new { Message = "Datos obtenidos con exito", Data = soccergames, Status = 200 });
         }
+
+        [HttpGet("{id}")]
+       // [Route("GamesByUser")]
+        public IActionResult GetGamesByUser(Guid id)
+        {
+
+            var soccergames = _db.Partidos.Where(v => v.UsuarioId == id).Join(_db.Usuarios,
+            game => game.UsuarioId,
+            user => user.UsuarioId,
+            (game, user) => new
+            {
+                PartidoId = game.PartidoId,
+                MaximoJugadores = game.MaximoJugadores,
+                TipoCancha = game.TipoCancha,
+                Fecha = game.Fecha,
+                Hora = game.Hora,
+                UsuarioId = game.UsuarioId,
+                NombreUsuario = user.NombreUsuario,
+            }
+            );
+
+
+            return Ok(new { Message = "Datos obtenidos con exito", Data = soccergames, Status = 200 });
+        }
+
 
         [HttpPost]
         public IActionResult Post([FromBody] PartidoDTO req)
         {
-         
-
-           /* var soccerGame = new Partido
+            var soccerGame = new Partido
             {
-                Nombre = req.Nombre,
-                Direccion = req.Direccion,
-                Estado = req.Estado
+                MaximoJugadores = req.MaximoJugadores,
+                TipoCancha = req.TipoCancha,
+                Fecha = req.Fecha,
+                Hora = req.Hora,
+                UsuarioId= req.UsuarioId,
             };
 
-            _db.Empresas.Add(company);
-            _db.SaveChanges();*/
+            _db.Partidos.Add(soccerGame);
+            _db.SaveChanges();
 
-            return Ok(new { Message = "Se creo la empresa", Data = ' ', Status = 201 });
+            return Ok(new { Message = "Se creo el partido", Data = soccerGame, Status = 201 });
         }
 
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var game = _db.Partidos.Find(id);
+            if (game == null)
+            {
+                return NotFound(new { Message = "No se encontro el partido", Data = ' ', Status = 404 });
+            }
 
+            _db.Partidos.Remove(game);
+            _db.SaveChanges();
+
+            return Ok(new { Message = "Se elimino el partido", Data = ' ', Status = 200 });
+        }
     }
 }
