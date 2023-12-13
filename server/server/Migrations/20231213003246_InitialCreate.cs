869c91dc0e906d4bf3527ba50520e3867e60ddc6
eljosema505 <eljosema505@gmail.com>
2023-12-12 20:44:39 -0400

    bk:create controller soccerGame

---
+++ server/server/Migrations/20231213003246_InitialCreate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "usuario",
+                columns: table => new
+                {
+                    usuario_id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    nombre_usuario = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    contrasenia = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("usuario_pkey", x => x.usuario_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "partidos",
+                columns: table => new
+                {
+                    partido_id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    maximo_jugadores = table.Column<int>(type: "integer", nullable: false),
+                    tipo_cancha = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    fecha = table.Column<DateOnly>(type: "date", nullable: false),
+                    hora = table.Column<TimeOnly>(type: "time without time zone", nullable: false),
+                    usuario_id = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("partidos_pkey", x => x.partido_id);
+                    table.ForeignKey(
+                        name: "partidos_usuario_id_fkey",
+                        column: x => x.usuario_id,
+                        principalTable: "usuario",
+                        principalColumn: "usuario_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_partidos_usuario_id",
+                table: "partidos",
+                column: "usuario_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "partidos");
+
+            migrationBuilder.DropTable(
+                name: "usuario");
+        }
+    }
+}
